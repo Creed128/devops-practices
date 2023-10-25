9c18e6d2c6d008bdaceb49d050e2666eba1d0c27
Credo <credongoukeng@gmail.com>
2023-10-25 10:06:29 +0000

    neu struktur auf aufgabe_5

--- 01_Linux/CLI-Übung/Aufgabe_5/begruessung.sh
+++ 01_Linux/CLI-Übung/Aufgabe_5/begruessung.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-
-# Begrüßung in bash script 
+#####################################################################
+# Script: aufgabe_5
+# es soll  ein Shell-Skript mit dem Namen "begruessung.sh" geschriben werden ,  
+#  das "Hallo, <DEIN Name>!" ausgibt, wobei <DEIN NAME> durch den Wert 
+# der gesetzten Umgebungsvariable ersetzt wird.
+#es soll die Möglichkeit, einen Begrüßungstext einzugen 
+# als Parameter zu akzeptieren, und passen Sie die Ausgabe entsprechend an. - dokumentiere auch deine Eingabe
+#
+# Autor: Credo Ngoukeng
+# Datum: 25.10.2023
+#####################################################################
 
 # Überprüfe, ob  MEIN_NAME gesetzt ist
 if [ -z "$MEIN_NAME" ]; then
