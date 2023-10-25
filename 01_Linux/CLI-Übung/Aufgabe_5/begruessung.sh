78f6ab187ae1902f92311fa1e3d14c58d4a2434f
Credo <credongoukeng@gmail.com>
2023-10-25 09:53:12 +0000

    begrüßung ohne text als parameter

---
+++ 01_Linux/CLI-Übung/Aufgabe_5/begruessung.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Begrüßung in bash script 
+
+# Überprüfe, ob  MEIN_NAME gesetzt ist
+if [ -z "$MEIN_NAME" ]; then
+  echo "Die Umgebungsvariable MEIN_NAME ist nicht gesetzt."
+else
+  echo "Hallo, $MEIN_NAME!"
+fi
+
+# Überprüfe, ob  Begrüßungstext als parameter übergeben ist 
+if [ $# -gt 0 ]; then
+  echo "ein wurde Begrüßungstext angegeben: $1"
+fi
