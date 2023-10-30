9cf9a929ddf754696b1f92dbcb5714118c98c749
Credo <credongoukeng@gmail.com>
2023-10-30 19:45:23 +0000

    test backup durgeführt

--- 01_Linux/CLI-Übung/Aufgabe_6/Backup-Skript/backup_script.sh
+++ 01_Linux/CLI-Übung/Aufgabe_6/Backup-Skript/backup_script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
 # script für backup
-# pfad für datei Quelle  definiert : 
-source_dir = "/home/vagrant/Heager-Consulting-Training/coacheeCredo/01_Linux/CLI-Übung/Aufgabe_6/Backup-Skript"
-# pfad zum backup verzeichniss 
-backup_dir ="/home/vagrant/Heager-Consulting-Training/coacheeCredo/01_Linux/CLI-Übung/Aufgabe_6/Backup-Skript/Backup"
-# Zeitstempel für den Dateinamen 
+# pfad für datei Quelle  definiert:
+source_dir="/home/vagrant/Heager-Consulting-Training/coacheeCredo/01_Linux/CLI-Übung/Aufgabe_6/Backup-Skript"
+# pfad zum backup verzeichniss
+backup_dir="/home/vagrant/Heager-Consulting-Training/coacheeCredo/01_Linux/CLI-Übung/Aufgabe_6/Backup-Skript/Backup"
+# Zeitstempel für den Dateinamen
 timestamp=$(date +%Y%m%d_%H%M%S)
+# datein name nach backup mit zeitstempel
+backup_filename="back_$timestamp.tar.gz"
+# backup durchführen und archivieren
+tar -czvf "$backup_dir/$backup_filename" -C "$source_dir" .
