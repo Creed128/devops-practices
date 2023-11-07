c5e6ca72a273f0fc12aa88cad3595c32e1666dac
Credo <credongoukeng@gmail.com>
2023-11-07 11:57:00 +0000

    speicherbelegung

--- 01_Linux/CLI-Übung/Systemüberwachung/sckript.sh
+++ 01_Linux/CLI-Übung/Systemüberwachung/sckript.sh
@@ -4,6 +4,11 @@
 echo "Systemüberwachung"
 echo "system start "
 # CPU Auslastung anzeigen 
-ech "Auslastung Ihre  sytem"
-CPU_auslastung=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}')
-echo "CPU-Auslastung: $cpu-auslastung%"
+echo "Auslastung Ihre  sytem"
+cpu_auslastung=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}')
+echo "CPU-Auslastung: $cpu_auslastung%"
+
+# Speicherbelegung hier anzeigen 
+Speicher_belegung=$(free -m | awk 'NR==2{print $3}')
+gesamt_speicer=(free -m | awk 'NR==2{print $2}')
+echo "Speicherbelegung: $speicher_belegung MB von $gesamt_speicher" MB verwendet
