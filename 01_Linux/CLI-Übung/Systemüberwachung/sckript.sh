22ee7f0e09f5d4bf40183479dfcf332c7abe64ca
Credo <credongoukeng@gmail.com>
2023-11-07 12:00:30 +0000

    speicher belegung anzeigen lassen

--- 01_Linux/CLI-Übung/Systemüberwachung/sckript.sh
+++ 01_Linux/CLI-Übung/Systemüberwachung/sckript.sh
@@ -9,6 +9,6 @@ cpu_auslastung=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}')
 echo "CPU-Auslastung: $cpu_auslastung%"
 
 # Speicherbelegung hier anzeigen 
-Speicher_belegung=$(free -m | awk 'NR==2{print $3}')
-gesamt_speicer=(free -m | awk 'NR==2{print $2}')
+speicher_belegung=$(free -m | awk 'NR==2{print $3}')
+gesamt_speicher=$(free -m | awk 'NR==2{print $2}')
 echo "Speicherbelegung: $speicher_belegung MB von $gesamt_speicher" MB verwendet
