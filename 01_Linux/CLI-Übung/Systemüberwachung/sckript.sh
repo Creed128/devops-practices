796ccc63687e6ef7883992f251a8e2f5424ef4ad
Credo <credongoukeng@gmail.com>
2023-11-07 12:14:57 +0000

    die Hardware anzeigen

--- 01_Linux/CLI-Übung/Systemüberwachung/sckript.sh
+++ 01_Linux/CLI-Übung/Systemüberwachung/sckript.sh
@@ -2,7 +2,7 @@
 #Systemüberwachung
 
 echo "Systemüberwachung"
-echo "system start "
+echo "--------------START---------------------- "
 # CPU Auslastung anzeigen 
 echo "Auslastung Ihre  sytem"
 cpu_auslastung=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}')
@@ -12,3 +12,9 @@ echo "CPU-Auslastung: $cpu_auslastung%"
 speicher_belegung=$(free -m | awk 'NR==2{print $3}')
 gesamt_speicher=$(free -m | awk 'NR==2{print $2}')
 echo "Speicherbelegung: $speicher_belegung MB von $gesamt_speicher" MB verwendet
+
+#Hardware anzeigen
+echo "Die angelschlossene Hardware sind : "
+lshw | grep -E "description|product|vendor|serial" | sed 's/^[ \t]*//'
+
+echo "-----------------------ENDE--------------------------"
