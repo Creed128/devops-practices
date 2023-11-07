47cf355eacb10eea774f0113fe827310e09e0ad4
Credo <credongoukeng@gmail.com>
2023-11-07 11:41:07 +0000

    Auslastung anzeigen

--- 01_Linux/CLI-Übung/Systemüberwachung/sckript.sh
+++ 01_Linux/CLI-Übung/Systemüberwachung/sckript.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 #Systemüberwachung
+
+echo "Systemüberwachung"
+echo "system start "
+# CPU Auslastung anzeigen 
+ech "Auslastung Ihre  sytem"
+CPU_auslastung=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}')
+echo "CPU-Auslastung: $cpu-auslastung%"
