8d72e05ced0a73344393916c8b66484452ca6e3a
Credo Ngoukeng <credongoukeng@gmail.com>
2024-01-19 11:19:42 +0100

    create subnetz

--- 04_Terraform/modules/vpc/main.tf
+++ 04_Terraform/modules/vpc/main.tf
@@ -6,3 +6,12 @@ resource "aws_vpc" "main" {
     Name = "main-vpc"
   }
 }
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zone
+  tags = {
+    Name = "public-subnet"
+  }
+}
