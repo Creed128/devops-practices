0f82409aea730b86a9fd73e9520a1d9e23ccf3f4
Credo Ngoukeng <credongoukeng@gmail.com>
2024-01-25 22:14:46 +0100

    update vp/outputs

--- 04_Terraform/modules/vpc/outputs.tf
+++ 04_Terraform/modules/vpc/outputs.tf
@@ -12,3 +12,14 @@ output "security_group_id" {
   description = "The ID of the security group"
   value       = aws_security_group.web_sg.id
 }
+output "vpc_id" {
+  description = "The ID of the VPC."
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  description = "The ID of the public subnet."
+  value       = aws_subnet.public.id
+}
+
+# Add other outputs as needed
