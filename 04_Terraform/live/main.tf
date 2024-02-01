4d6c2f19612760cdbb7d0fa94a9343939183547d
Credo Ngoukeng <credongoukeng@gmail.com>
2024-02-01 08:17:19 +0100

    terraform plan runing

--- 04_Terraform/live/main.tf
+++ 04_Terraform/live/main.tf
@@ -4,11 +4,13 @@ provider "aws" {
 
 module "vpc" {
   source = "../modules/vpc"
+
   vpc_cidr            = "10.0.0.0/16"
   public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
   availability_zones  = ["eu-central-1a", "eu-central-1b"]
   vpc_name            = "mon-vpc"
 }
+
 module "webserver" {
   source = "../modules/webserver"
 
@@ -16,13 +18,15 @@ module "webserver" {
   instance_type     = "t2.micro"
   min_size          = 2
   max_size          = 4
-  vpc_id            = module.vpc.vpc_id  # Assurez-vous que cette ligne est correcte
+  vpc_id            = module.vpc.vpc_id
   subnet_ids        = module.vpc.public_subnet_ids
   security_group_id = module.vpc.webserver_sg_id
 }
 
 module "loadbalancer" {
   source = "../modules/loadbalancer"
+
+  vpc_id               = module.vpc.vpc_id
   subnet_ids           = module.vpc.public_subnet_ids
   lb_security_group_id = module.vpc.lb_sg_id
   target_group_arn     = module.webserver.webserver_tg_arn
