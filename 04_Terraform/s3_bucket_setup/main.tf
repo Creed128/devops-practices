a7f2743cf1b46dac6b2dc0c3af3c073bad7267f6
Credo Ngoukeng <credongoukeng@gmail.com>
2024-02-06 13:01:57 +0100

    update add alb_log_polic

--- 04_Terraform/s3_bucket_setup/main.tf
+++ 04_Terraform/s3_bucket_setup/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
   acl    = "private"
@@ -6,7 +10,46 @@ resource "aws_s3_bucket" "terraform_state" {
     enabled = true
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "alb_log_policy" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        },
+        Action = "s3:PutObject",
+        Resource = "arn:aws:s3:::${var.bucket_name}/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      },
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        },
+        Action = "s3:GetBucketAcl",
+        Resource = "arn:aws:s3:::${var.bucket_name}"
+      }
+    ]
+  })
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
